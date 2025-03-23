9e3415a64773d9d96dd2e44f3cb844dc308156ff
Wandini <h.wandile@wustl.edu>
2025-03-23 15:57:48 -0500

    Trying to fix the issue

---
+++ src/bankingapp/AccountHolder.java
@@ -0,0 +1,50 @@
+package bankingapp;
+import java.util.ArrayList;
+public class AccountHolder {
+	private final String first_name;
+	
+	private final String lastname;
+	
+	private final int birthday;
+	
+	private final int ssn;
+	
+	private final int BankCode;
+	
+	private ArrayList<Integer> BankAcounts;
+
+	public AccountHolder(){
+		this.first_name = null;
+		this.lastname = null;
+		this.birthday = 0;
+		this.ssn = 0 ;
+		this.BankAcounts = new ArrayList<Integer>();
+		this.BankCode = 0;
+	}
+	
+	public void addBankacount(AccountHolder info, int BankAcountCode  ) {
+		if(!info.BankAcounts.contains(BankAcountCode)) {
+			info.BankAcounts.add(BankCode);
+		}
+	}
+	
+	public void removeBankacount(AccountHolder info, int BankAcountCode  ) {
+		if(info.BankAcounts.contains(BankAcountCode)) {
+			info.BankAcounts.remove(BankCode);
+		}
+	}
+	
+	public void listBankAcounts(AccountHolder info){
+		int i =1;
+		for(Integer list: info.BankAcounts) {
+			System.out.printf("Saving Account" + i, list);
+			i++;
+		}
+	}
+	
+	public boolean findBankAccount(AccountHolder info, int BankAcountCode) {
+		return info.BankAcounts.contains(BankAcountCode);
+	}
+	
+
+}
