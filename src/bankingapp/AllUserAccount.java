cb0655c234bd879d9e1a3bdeff5d39a433cf91a2
Wandini <h.wandile@wustl.edu>
2025-03-23 16:12:38 -0500

    Made a Bank Account Library

--- src/bankingapp/AllUserAccount.java
+++ src/bankingapp/AllUserAccount.java
@@ -1,5 +1,29 @@
 package bankingapp;
 
-public class AllUserAccount {
+import java.util.HashMap;
 
+public class AllUserAccount {
+	private HashMap<AccountHolder, Integer> Useraccounts = new HashMap<>();
+	
+	public int AccountNumber(AccountHolder info) {
+		int hashcode = info.hashCode();
+		return hashcode;
+	}
+	
+	public void AddAcount(AccountHolder info) {
+		int hash = AccountNumber(info);
+		if(!Useraccounts.containsKey(hash)) {
+			Useraccounts.put(info,hash);
+		}
+	}
+	
+	public boolean findAcount( int hash) {
+		return Useraccounts.containsKey(hash);
+	}
+	
+	public void deletAccount(AccountHolder info, int hash) {
+		if(Useraccounts.containsKey(hash)) {
+			Useraccounts.put(info,hash);
+		}
+	}
 }
---
+++ src/bankingapp/BankAccountLibrary.java
@@ -0,0 +1,28 @@
+package bankingapp;
+import java.util.HashMap;
+public class BankAccountLibrary {
+	
+	private HashMap<BankAccount, Integer> BankAccountLibrary = new HashMap<>();
+	
+	public int BankAccountNumber(BankAccount info) {
+		int hashcode = info.hashCode();
+		return hashcode;
+	}
+	
+	public void AddAcount(BankAccount info) {
+		int hash = BankAccountNumber(info);
+		if(!BankAccountLibrary.containsKey(hash)) {
+			BankAccountLibrary.put(info,hash);
+		}
+	}
+	
+	public boolean findAcount( int hash) {
+		return BankAccountLibrary.containsKey(hash);
+	}
+	
+	public void deletAccount(BankAccount info, int hash) {
+		if(BankAccountLibrary.containsKey(hash)) {
+			BankAccountLibrary.put(info,hash);
+		}
+	}
+}
