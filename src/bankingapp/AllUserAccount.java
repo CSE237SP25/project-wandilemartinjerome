8b3987b8a2edb52cfdad4b53f5e7546552a20156
Wandini <h.wandile@wustl.edu>
2025-03-24 10:33:22 -0500

    User Accounts added

--- src/bankingapp/AllUserAccount.java
+++ src/bankingapp/AllUserAccount.java
@@ -1,5 +1,29 @@
 package bankingapp;
 
-public class AllUserAccount {
+import java.util.HashMap;
 
+public class AllUserAccount {
+	private HashMap<AccountHolder, Integer> UserAccounts = new HashMap<>();
+	
+	public int AccountNumber(AccountHolder info) {
+		int hashcode = info.hashCode();
+		return hashcode;
+	}
+	
+	public void AddAcount(AccountHolder info) {
+		int hash = AccountNumber(info);
+		if(!UserAccounts.containsKey(hash)) {
+			UserAccounts.put(info,hash);
+		}
+	}
+	
+	public boolean findAcount( int hash) {
+		return UserAccounts.containsKey(hash);
+	}
+	
+	public void deletAccount(AccountHolder info, int hash) {
+		if(UserAccounts.containsKey(hash)) {
+			UserAccounts.put(info,hash);
+		}
+	}
 }
