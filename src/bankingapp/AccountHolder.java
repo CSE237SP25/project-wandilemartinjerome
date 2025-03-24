15af94ac5e933e9373effbbc83105c12cb269482
Jerome Hsing <82135305+jhsing24@users.noreply.github.com>
2025-03-24 10:02:57 -0500

    Fixed spelling issues

--- src/bankingapp/AccountHolder.java
+++ src/bankingapp/AccountHolder.java
@@ -9,38 +9,38 @@ public class AccountHolder {
 	
 	private final int BankCode;
 	
-	private ArrayList<Integer> BankAcounts;
+	private ArrayList<Integer> BankAccounts;
 
 	public AccountHolder(){
 		this.lastname = null;
 		this.birthday = 0;
 		this.ssn = 0 ;
-		this.BankAcounts = new ArrayList<Integer>();
+		this.BankAccounts = new ArrayList<Integer>();
 		this.BankCode = 0;
 	}
 	
 	public void addBankacount(AccountHolder info, int BankAccountCode  ) {
-		if(!info.BankAcounts.contains(BankAccountCode)) {
-			info.BankAcounts.add(BankAccountCode);
+		if(!info.BankAccounts.contains(BankAccountCode)) {
+			info.BankAccounts.add(BankCode);
 		}
 	}
 	
 	public void removeBankacount(AccountHolder info, int BankAccountCode  ) {
-		if(info.BankAcounts.contains(BankAccountCode)) {
-			info.BankAcounts.remove(BankAccountCode);
+		if(info.BankAccounts.contains(BankAccountCode)) {
+			info.BankAccounts.remove(BankCode);
 		}
 	}
 	
-	public void listBankAcounts(AccountHolder info){
+	public void listBankAccounts(AccountHolder info){
 		int i =1;
-		for(Integer list: info.BankAcounts) {
+		for(Integer list: info.BankAccounts) {
 			System.out.printf("Saving Account" + i, list);
 			i++;
 		}
 	}
 	
 	public boolean findBankAccount(AccountHolder info, int BankAccountCode) {
-		return info.BankAcounts.contains(BankAccountCode);
+		return info.BankAccounts.contains(BankAccountCode);
 	}
 	
 
