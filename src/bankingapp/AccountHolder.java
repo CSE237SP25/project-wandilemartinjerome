fabd9b4fb01eac314ef092cce2d225145d0b59fd
Jerome Hsing <82135305+jhsing24@users.noreply.github.com>
2025-04-07 11:42:00 -0500

    Added getAccount to allow for password protected personal information

--- src/bankingapp/AccountHolder.java
+++ src/bankingapp/AccountHolder.java
@@ -2,22 +2,128 @@ package bankingapp;
 import java.util.ArrayList;
 import java.util.HashMap;
 public class AccountHolder {
-	private final String lastname;
+	private String lastname;
 	
-	private final String birthday;
+	private String birthday;
 	
-	private final int ssn;
+	private int ssn;
 	
-	private final int BankCode;
+	private int BankCode;
 	
 	private ArrayList<Integer> BankAccounts;
+	
+	private String password; // Password to protect personal information
+	
+	private boolean isPersonalInfoHidden; // Flag to track if personal info is hidden
 
 	public AccountHolder(){
 		this.lastname = null;
 		this.birthday = null;
-		this.ssn = 0 ;
+		this.ssn = 0;
 		this.BankAccounts = new ArrayList<Integer>();
 		this.BankCode = 0;
+		this.password = null;
+		this.isPersonalInfoHidden = false;
+	}
+	
+	/**
+	 * Creates a new account holder with personal information
+	 * 
+	 * @param lastname The account holder's last name
+	 * @param birthday The account holder's birthday
+	 * @param ssn The account holder's social security number
+	 * @param bankCode The bank code
+	 */
+	public AccountHolder(String lastname, String birthday, int ssn, int bankCode) {
+		this.lastname = lastname;
+		this.birthday = birthday;
+		this.ssn = ssn;
+		this.BankCode = bankCode;
+		this.BankAccounts = new ArrayList<Integer>();
+		this.password = null;
+		this.isPersonalInfoHidden = false;
+	}
+	
+	/**
+	 * Sets the account holder's personal information
+	 * 
+	 * @param lastname The account holder's last name
+	 * @param birthday The account holder's birthday
+	 * @param ssn The account holder's social security number
+	 * @param bankCode The bank code
+	 */
+	public void setPersonalInfo(String lastname, String birthday, int ssn, int bankCode) {
+		this.lastname = lastname;
+		this.birthday = birthday;
+		this.ssn = ssn;
+		this.BankCode = bankCode;
+	}
+	
+	/**
+	 * Sets a password to protect personal information
+	 * 
+	 * @param password The password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/**
+	 * Hides personal information behind password protection
+	 */
+	public void hidePersonalInfo() {
+		this.isPersonalInfoHidden = true;
+	}
+	
+	/**
+	 * Shows personal information if correct password is provided
+	 * 
+	 * @param inputPassword The password to check
+	 * @return true if password is correct and personal info is now visible, false otherwise
+	 */
+	public boolean showPersonalInfo(String inputPassword) {
+		if (this.password != null && this.password.equals(inputPassword)) {
+			this.isPersonalInfoHidden = false;
+			return true;
+		}
+		this.isPersonalInfoHidden = true; // Ensure info stays hidden with wrong password
+		return false;
+	}
+	
+	/**
+	 * Gets personal information if not hidden or if correct password is provided
+	 * 
+	 * @param inputPassword The password to check
+	 * @return A string containing personal information, or null if hidden and wrong password
+	 */
+	public String getPersonalInfo(String inputPassword) {
+		// Always require password if personal info is hidden
+		if (isPersonalInfoHidden) {
+			if (this.password != null && this.password.equals(inputPassword)) {
+				return "Last Name: " + lastname + "\n" +
+					   "Birthday: " + birthday + "\n" +
+					   "SSN: " + ssn + "\n" +
+					   "Bank Code: " + BankCode;
+			}
+			return null;
+		}
+		
+		// If not hidden, still require password if one is set
+		if (this.password != null) {
+			if (this.password.equals(inputPassword)) {
+				return "Last Name: " + lastname + "\n" +
+					   "Birthday: " + birthday + "\n" +
+					   "SSN: " + ssn + "\n" +
+					   "Bank Code: " + BankCode;
+			}
+			return null;
+		}
+		
+		// Only return info without password if no password is set and info is not hidden
+		return "Last Name: " + lastname + "\n" +
+			   "Birthday: " + birthday + "\n" +
+			   "SSN: " + ssn + "\n" +
+			   "Bank Code: " + BankCode;
 	}
 	
 	public void addBankAccount(AccountHolder info, int BankAccountCode  ) {
--- src/tests/TransactionHistoryTest.java
+++ src/tests/TransactionHistoryTest.java
@@ -13,23 +13,24 @@ public class TransactionHistoryTest {
         BankAccount account = new BankAccount(100);
         account.deposit(50);
         account.withdraw(25);
-
-    List<Transaction> history = account.getTransactionHistory();
-    assertEquals(3, history.size()); // Initial deposit + deposit + withdrawal
     
-    // Check the first transaction (initial deposit)
-    Transaction initialDeposit = history.get(0);
-    assertEquals(TransactionType.DEPOSIT, initialDeposit.getType());
-    assertEquals(100, initialDeposit.getAmount(), 0.001);
-    assertEquals("Initial deposit", initialDeposit.getDescription());
+        List<Transaction> history = account.getTransactionHistory();
+        assertEquals(3, history.size()); // Initial deposit + deposit + withdrawal
+        
+        // Check the first transaction (initial deposit)
+        Transaction initialDeposit = history.get(0);
+        assertEquals(TransactionType.DEPOSIT, initialDeposit.getType());
+        assertEquals(100, initialDeposit.getAmount(), 0.001);
+        assertEquals("Initial deposit", initialDeposit.getDescription());
 
-    // Check the second transaction (deposit)
-    Transaction deposit = history.get(1);
-    assertEquals(TransactionType.DEPOSIT, deposit.getType());
-    assertEquals(50, deposit.getAmount(), 0.001);
+        // Check the second transaction (deposit)
+        Transaction deposit = history.get(1);
+        assertEquals(TransactionType.DEPOSIT, deposit.getType());
+        assertEquals(50, deposit.getAmount(), 0.001);
 
-    // Check the third transaction (withdrawal)
-    Transaction withdrawal = history.get(2);
-    assertEquals(TransactionType.WITHDRAWAL, withdrawal.getType());
-    assertEquals(25, withdrawal.getAmount(), 0.001);
+        // Check the third transaction (withdrawal)
+        Transaction withdrawal = history.get(2);
+        assertEquals(TransactionType.WITHDRAWAL, withdrawal.getType());
+        assertEquals(25, withdrawal.getAmount(), 0.001);
+    }
 }
